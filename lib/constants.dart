48afdefcacdae1e8bbeb2774daa4c8d0d9d288ff
Struzy <manuel.struzyna@outlook.de>
2022-11-26 18:58:17 +0100

    Fonts and new files added

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// SGM colors
+const Color kSGMColorGreen = Color(0xFF559A56);
+const Color kSGMColorBlue = Color(0xFF3A83DF);
+const Color kSGMColorRed = Color(0xFFEA343A);
